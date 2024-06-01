e50dc29c83902af92996f51e60faa7daef15c116
Peter Gabriel <peternex42@gmail.com>
2024-06-01 11:00:43 +0800

    create portfolio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,180 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Inter, Poppins, Anton, Roboto } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const poppins = Poppins({ subsets: ["latin"], weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"] });
+
+const anton = Anton({ subsets: ["latin"], weight: ["400"] });
+
+const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"] });
+
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      id="home"
+      className={`w-full min-h-screen mb-4 ${poppins.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+      <Image
+        src={"/EllipseTop.png"}
+        width={450}
+        height={450}
+        className="absolute top-0 -z-10"
+      />
+      <Image
+        src={"/EllipseBottom.png"}
+        width={450}
+        height={450}
+        className="absolute right-0 top-3/4 -z-10"
+      />
+
+      <div className="px-10">
+        <div className="mt-8 w-full">
+          <div className="px-4 flex flex-row justify-between items-center w-full bg-[#EADBC8] h-[94px] rounded-3xl drop-shadow-md">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src={"/LogoGabsee.png"}
+              width={90}
+              height={90}
             />
-          </a>
+            <div className="flex flex-row gap-[90px] pr-8 text-[#000000] text-[24px]">
+              <a href="#hero"><span>Home</span></a>
+              <a href="#about"><span>About</span></a>
+              <a href="#projects"><span>Projects</span></a>
+              <a href="#contact"><span>Contact</span></a>
+            </div>
+          </div>
         </div>
+
+        <div className="h-56"></div>
+
+        <div id="hero" className="flex w-full h-[565px] bg-[#DAC0A3] rounded-[48px]">
+          <Image
+            src={"/gabsee.png"}
+            width={523}
+            height={758}
+            className=" absolute -bottom-[142px] left-[100px]"
+          />
+          <div className="w-full flex flex-col justify-center items-end gap-4 pr-12">
+            <p className={`${anton.className} w-1/2 text-black text-end text-[120px] font-bold leading-[120px]`}>PETER GABRIEL SOMPOTAN</p>
+            <p className={`${roboto.className}  text-black font-medium text-[24px] `}>Frontend Developer & UI UX Designer</p>
+            <p className="w-[529px] text-black text-end  right-10 top-[460px]">I create mobile and web interfaces that delight users and keep them coming back. I prioritize intuitive navigation, seamless interactions, and responsive design. I believe that a great user experience is the key to a successful product.</p>
+          </div>
+
+        </div>
+
+        <div className="flex flex-row items-center justify-center">
+          <Image
+            src={"/unity-text.png"}
+            width={200}
+            height={200}
+          />
+          <Image
+            src={"/logo-unsrat.png"}
+            width={140}
+            height={140}
+          />
+        </div>
+
+        <div className="flex flex-col gap-5">
+          <div className="flex flex-row gap-2  ">
+            <div className="p-20 w-2/3 h-[644px] bg-[#DAC0A3] rounded-[48px] drop-shadow-xl">
+              <p className={`${anton.className} text-black text-[100px] leading-[120px]`}>I DELIVER RESULTS THAT MAKE A DIFFERENCE</p>
+              <p className={`${poppins.className} text-black pr-2`}>I don't just write code, I build solutions that drive user engagement, increase conversions, and improve your bottom line.</p>
+            </div>
+            <Image
+              src={"/gabsee2.JPG"}
+              width={486}
+              height={644}
+              className="w-1/3 rounded-[48px] drop-shadow-xl"
+            />
+          </div>
+
+
+          <div id="projects" className="w-full bg-[#DAC0A3] rounded-[48px] h-[500px] flex flex-row items-center justify-center drop-shadow-xl">
+            <div className="p-14 w-full text-black flex flex-col gap-1">
+              <p className={`${anton.className} w-[485px] text-[100px] leading-[120px] `}>REPORT FORMATTER</p>
+              <p className={`${poppins.className} font-medium text-[24px]`}>UI UX Design</p>
+              <p className={`${poppins.className} w-[485px] leading-[20px] `}>Report Formatter is an app that helps student to create their dissertation, you don’t have to worry about the indentation, what font does it use in common, everything makes easy using this app.</p>
+            </div>
+            <div>
+              <Image
+                src={"/macbook-pict.png"}
+                width={765}
+                height={510}
+                className="rounded-[48px] mr-[500px]"
+              />
+            </div>
+
+          </div>
+
+          <div className="w-full bg-[#DAC0A3] rounded-[48px] h-[500px] flex flex-row items-center justify-center drop-shadow-xl">
+            <div className="p-14 w-1/2 text-black flex flex-col gap-1">
+              <p className={`${anton.className} w-[600px] text-[100px] leading-[120px] `}>COURSE ABSENCE CODE</p>
+              <p className={`${poppins.className} font-medium text-[24px]`}>UI UX Design</p>
+              <p className={`${poppins.className} w-[485px] leading-[20px] `}>This app is for campus activities helper. So when a lecturer wan’t absence their presence they just give a code and student fill it on this app.</p>
+            </div>
+            <div className="w-1/2 h-full">
+              <Image
+                src={"/mockup-android.png"}
+                width={500}
+                height={500}
+                className="rounded-[48px] w-full h-full "
+              />
+            </div>
+
+          </div>
+
+          <div id="about" className="flex flex-row-reverse gap-2  ">
+            <div className="p-20 w-2/3 h-[644px] bg-[#DAC0A3] rounded-[48px] flex flex-col gap-4 drop-shadow-xl">
+              <p className={`${anton.className} text-black text-[90px] leading-[90px]`}>FRONTEND DEVELOPER & UI UX DESIGNER ENTHUSIAST</p>
+              <p className={`${poppins.className} text-black pr-2`}>Passionate about web & mobile ui technologies. I love working at the intersection of user friendly interfaces and creativity. I create memorable web & mobile experiences.</p>
+              <p className={`${poppins.className} text-black pr-2`}>When i’m not building or exploring new web & mobile experiences, i’m probably reading books or go for a walk.</p>
+            </div>
+            <Image
+              src={"/gabsee3.JPG"}
+              width={486}
+              height={644}
+              className="w-1/3 rounded-[48px] drop-shadow-xl"
+            />
+          </div>
+
+          <div id="contact" className="w-full bg-[#DAC0A3] rounded-[48px] h-[350px] flex flex-col justify-center items-center drop-shadow-xl">
+            <p className={`${anton.className} text-black text-[90px] `}>CONTACT ME</p>
+            <div className="flex flex-row gap-12">
+              <a href="mailto:peternex42@gmail.com">
+                <Image
+                  src={"/gmail.png"}
+                  width={100}
+                  height={100}
+                />
+              </a>
+              <a href="https://www.linkedin.com/in/gabsee">
+                <Image
+                  src={"/linkedin.png"}
+                  width={100}
+                  height={100}
+                />
+              </a>
+              <a href="https://www.instagram.com/petersompotan/">
+                <Image
+                  src={"/instagram.png"}
+                  width={100}
+                  height={100}
+                />
+              </a>
+
+            </div>
+          </div>
+
+
+        </div>
+
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,11 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      inter: ["Inter", "latin"],
+      poppins: ["Poppins", "latin"],
+      anton: ["Anton", "latin"]
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
